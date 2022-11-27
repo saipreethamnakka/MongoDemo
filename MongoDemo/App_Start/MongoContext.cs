a871bdbf506aa8871cc54d7b694a1c0b8d89a5e5
NAKKA SAIPREETHAM <NAKKA SAIPREETHAM@DESKTOP-6TBAHVH>
2022-11-27 23:22:35 +0530

    Add project files.

---
+++ MongoDemo/App_Start/MongoContext.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Configuration;
+
+namespace MongoDemo.App_Start
+{
+    public class MongoContext
+    {
+        MongoClient _client;
+        MongoServer _server;
+        public MongoDatabase _database;
+        public MongoContext()        //constructor   
+        {
+            // Reading credentials from Web.config file   
+            var MongoDatabaseName = ConfigurationManager.AppSettings["MongoDatabaseName"]; //CarDatabase  
+            var MongoUsername = ConfigurationManager.AppSettings["MongoUsername"]; //demouser  
+            var MongoPassword = ConfigurationManager.AppSettings["MongoPassword"]; //Pass@123  
+            var MongoPort = ConfigurationManager.AppSettings["MongoPort"];  //27017  
+            var MongoHost = ConfigurationManager.AppSettings["MongoHost"];  //localhost  
+
+            // Creating credentials  
+            var credential = MongoCredential.CreateMongoCRCredential
+                            (MongoDatabaseName,
+                             MongoUsername,
+                             MongoPassword);
+
+            // Creating MongoClientSettings  
+            var settings = new MongoClientSettings
+            {
+                Credentials = new[] { credential },
+                Server = new MongoServerAddress(MongoHost, Convert.ToInt32(MongoPort))
+            };
+            _client = new MongoClient(settings);
+            _server = _client.GetServer();
+            _database = _server.GetDatabase(MongoDatabaseName);
+        }
+    }
+}
